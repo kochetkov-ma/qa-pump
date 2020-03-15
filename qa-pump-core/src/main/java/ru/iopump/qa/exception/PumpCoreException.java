67106e4d2d4c70cb64643d4d2b2145c0f390747d
kochetkov-ma <kochetkov-ma@yandex.ru>
2020-03-15 21:24:50 +0300

    100% coverage

--- qa-pump-core/src/main/java/ru/iopump/qa/exception/PumpCoreException.java
+++ qa-pump-core/src/main/java/ru/iopump/qa/exception/PumpCoreException.java
@@ -1,44 +1,70 @@
 package ru.iopump.qa.exception;
 
 import javax.annotation.Nullable;
+import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import ru.iopump.qa.util.ClassUtil;
 
+/**
+ * No constructors. Only static constructors and 'with' methods witch return copy of this 'PumpCoreException'.
+ */
 @SuppressWarnings("unused")
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PumpCoreException extends QaException {
 
     private static final long serialVersionUID = 2540707445608794577L;
 
-    public PumpCoreException(String formattedMessage, Object... arguments) {
-        this(formattedMessage, null, arguments);
+    protected PumpCoreException(String formattedMessage, Object... arguments) {
+        super(formattedMessage, arguments);
     }
 
-    public PumpCoreException(String formattedMessage, Throwable cause, Object... arguments) {
+    protected PumpCoreException(String formattedMessage, Throwable cause, Object... arguments) {
         super(formattedMessage, cause, arguments);
     }
 
-    public PumpCoreException(Throwable cause) {
+    protected PumpCoreException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * Constructor.
+     */
     @NonNull
     public static PumpCoreException of() {
         return new PumpCoreException();
     }
 
+    /**
+     * Constructor.
+     */
+    @NonNull
+    public static PumpCoreException of(@Nullable Throwable cause) {
+        return cause == null ? of() : of().withCause(cause);
+    }
+
+    /**
+     * Constructor.
+     */
     @NonNull
     public static PumpCoreException of(@Nullable String formattedMessage, @Nullable Object... arguments) {
         return formattedMessage == null ? of() : new PumpCoreException(formattedMessage, arguments);
     }
 
+    /**
+     * New copy of this {@link PumpCoreException} with new message.
+     */
     @NonNull
     public PumpCoreException withMsg(@Nullable String formattedMessage, @Nullable Object... arguments) {
         return (PumpCoreException) of(formattedMessage, arguments).initCause(getCause());
     }
 
+    /**
+     * New copy of this {@link PumpCoreException} with new cause.
+     */
     @NonNull
-    public PumpCoreException withCause() {
-        return (PumpCoreException) of(getMessage()).initCause(getCause());
+    public PumpCoreException withCause(Throwable cause) {
+        return cause == null ? of(getMessage()) : (PumpCoreException) of(getMessage() == null ?
+            ClassUtil.getClass(cause).getName() : getMessage()).initCause(cause);
     }
 }
--- qa-pump-core/src/test/java/ru/iopump/qa/exception/PumpCoreExceptionTest.java
+++ qa-pump-core/src/test/java/ru/iopump/qa/exception/PumpCoreExceptionTest.java
@@ -7,6 +7,42 @@ import org.junit.Test;
 public class PumpCoreExceptionTest {
 
     @Test
+    public void create() {
+        assertThat(new PumpCoreException("", null, ""))
+            .isInstanceOf(PumpCoreException.class)
+            .hasMessage("")
+            .hasNoCause();
+
+        assertThat(new PumpCoreException(new RuntimeException()))
+            .isInstanceOf(PumpCoreException.class)
+            .hasMessage("java.lang.RuntimeException")
+            .hasCauseInstanceOf(RuntimeException.class);
+
+        assertThat(PumpCoreException.of(new RuntimeException()))
+            .isInstanceOf(PumpCoreException.class)
+            .hasMessage("java.lang.RuntimeException")
+            .hasCauseInstanceOf(RuntimeException.class);
+
+        assertThat(PumpCoreException.of().initCause(new RuntimeException()))
+            .isInstanceOf(PumpCoreException.class)
+            .hasMessage(null)
+            .hasCauseInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void ofCause() {
+        assertThat(PumpCoreException.of(new Exception()))
+            .isInstanceOf(PumpCoreException.class)
+            .hasMessage("java.lang.Exception")
+            .hasCauseInstanceOf(Exception.class);
+
+        assertThat(PumpCoreException.of(null))
+            .isInstanceOf(PumpCoreException.class)
+            .hasMessage(null)
+            .hasNoCause();
+    }
+
+    @Test
     public void of() {
         assertThat(PumpCoreException.of())
             .isInstanceOf(PumpCoreException.class)
@@ -30,5 +66,26 @@ public class PumpCoreExceptionTest {
             .isInstanceOf(PumpCoreException.class)
             .hasMessage(null)
             .hasNoCause();
+    }
+
+    @Test
+    public void withCause() {
+        assertThat(PumpCoreException.of().withCause(new IllegalArgumentException()))
+            .isInstanceOf(PumpCoreException.class)
+            .hasMessage("java.lang.IllegalArgumentException")
+            .hasCauseInstanceOf(IllegalArgumentException.class);
+
+        assertThat(PumpCoreException.of().withCause(null))
+            .isInstanceOf(PumpCoreException.class)
+            .hasMessage(null)
+            .hasNoCause();
+    }
+
+    @Test
+    public void withMsg() {
+        assertThat(PumpCoreException.of("no").withCause(new IllegalArgumentException()).withMsg("new"))
+            .isInstanceOf(PumpCoreException.class)
+            .hasMessage("new")
+            .hasCauseInstanceOf(IllegalArgumentException.class);
     }
 }
